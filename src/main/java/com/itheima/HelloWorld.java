545c363b2dae0900a7cd0a8412e02e4371af0249
inbytebeat <1515577847@qq.com>
2023-02-08 18:48:00 +0800

    四次次提交

--- src/main/java/com/itheima/HelloWorld.java
+++ src/main/java/com/itheima/HelloWorld.java
@@ -2,6 +2,6 @@ package com.itheima;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello Maven2~");
+        System.out.println("Hello Maven4~");
     }
 }
